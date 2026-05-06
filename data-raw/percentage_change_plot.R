99045425e68f8612848f5c32fdc2edf87f827baa
Bia Dias <bia.dias@noaa.gov>
2026-05-06 16:12:50 -0600

    launch

--- data-raw/percentage_change_plot.R
+++ data-raw/percentage_change_plot.R
@@ -4,15 +4,15 @@
 #AFFILIATIONS: CICOES University of Washington/ Alaska Fisheries Science Center
 #E-MAIL OF CORRESPONDENCE AUTHOR: bia.dias@noaa.gov
 #
-#R SCRIPT FOR PLOTTING PERCENT CHANGE BETWEEN ECOSYSTEM SCENARIOS OR MODELS UPDATES. 
-# The original code was written in Phyton by Evan, here is a translation by me. 
+#R SCRIPT FOR PLOTTING PERCENT CHANGE BETWEEN ECOSYSTEM SCENARIOS OR MODELS UPDATES.
+# The original code was written in Phyton by Evan, here is a translation by me.
 #------------------------------------------------------------------------------#
 
 
-# My idea is write this as a function so we can have percent change for old vs updated models and also percent change between scenarios. 
+# My idea is write this as a function so we can have percent change for old vs updated models and also percent change between scenarios.
 # The user is responsible for formatting their dt according to the structure bellow
 #  file structure = node, group_name, TL_base_model, biomass_base_model, TL_sc1, B_sc1, etc...
-#  file structure = node, group_name, TL_base_model, biomass_base_model, TL_updated_model, B_updated_model. 
+#  file structure = node, group_name, TL_base_model, biomass_base_model, TL_updated_model, B_updated_model.
 # I want to have 5 scenarios something like this:
 #
 # plot_bmsc_diffbd <- function(file_path, n_scenarios = 3) {
@@ -23,9 +23,9 @@
 library(tidyverse)
 theme_set(theme_minimal(base_family = "Arial"))
 
-#fname <- "C:/Users/biadias/Dropbox/EwE PhD BD/EwE M/ATL HERRING PAPER/ECOSIM/BaseModel_Scs_Diff.csv"
- 
-scenarios <- read_csv(fname)
+fname <- "C:/Users/biadias/Dropbox/EwE PhD BD/EwE M/ATL HERRING PAPER/ECOSIM/BaseModel_Scs_Diff.csv"
+
+scenarios <- read.csv(fname)
 
 #Rename columns # I could also use janitor::
 scenarios <- scenarios %>%
@@ -33,13 +33,13 @@ scenarios <- scenarios %>%
     Node     = Node,
     Group_name = Group_name,
     TL_BM    = "TL_BM",
-    B_BM     = "Biomass_t.km-2_BM",
+    B_BM     = "Biomass_t.km.2_BM",
     TL_SC1   = "TL_Sc1",
-    B_SC1    = "Biomass_t.km-2_Sc1",
+    B_SC1    = "Biomass_t.km.2_Sc1",
     TL_SC2   = "TL_Sc2",
-    B_SC2    = "Biomass_t.km-2_Sc2",
+    B_SC2    = "Biomass_t.km.2_Sc2",
     TL_SC3   = "TL_Sc3",
-    B_SC3    = "Biomass_t.km-2_Sc3"
+    B_SC3    = "Biomass_t.km.2_Sc3"
   )
 
 # Percent change in Biomass (dp_bm1, dp_bm2, dp_bm3) relative to the baseline (B_BM)
@@ -51,16 +51,16 @@ scenarios <- scenarios %>%
   )
 
 # I am renaming here, because my FG names are abbreviated
-bar_labels_all <- c("Phytoplankton", "Bacteria", "Microzooplankton", "Copepod-S", "Copepod-L", 
-                    "Gelatinous zooplankton", "Micronekton", "Macrobenthos polychaetae", 
-                    "Macrobenthos crustaceans", "Macrobenthos mollusks", "Other macrobenthos", 
-                    "Megabenthos filters", "Other megabenthos", "Shrimp", "Mesopelagics", 
-                    "Atlantic herring", "Alosine", "Atlantic mackerel", "Squid", "Butterfish", 
-                    "Small pelagics", "Bluefish", "Striped bass", "Dogfish-S", "Dogfish-L", 
-                    "Atlantic Cod-S", "Atlantic Cod-M", "Atlantic Cod-L", "Haddock", "Hake-S", 
-                    "Hake-L", "Yellow flounder", "Summer flounder", "Skate", "Demersal benthivores", 
-                    "Demersal piscivores", "Demersal omnivores", "Medium pelagics", "Pelagic sharks", 
-                    "Large pelagics (HMS)", "Pinnipeds", "Baleen whales", "Odontocetes", 
+bar_labels_all <- c("Phytoplankton", "Bacteria", "Microzooplankton", "Copepod-S", "Copepod-L",
+                    "Gelatinous zooplankton", "Micronekton", "Macrobenthos polychaetae",
+                    "Macrobenthos crustaceans", "Macrobenthos mollusks", "Other macrobenthos",
+                    "Megabenthos filters", "Other megabenthos", "Shrimp", "Mesopelagics",
+                    "Atlantic herring", "Alosine", "Atlantic mackerel", "Squid", "Butterfish",
+                    "Small pelagics", "Bluefish", "Striped bass", "Dogfish-S", "Dogfish-L",
+                    "Atlantic Cod-S", "Atlantic Cod-M", "Atlantic Cod-L", "Haddock", "Hake-S",
+                    "Hake-L", "Yellow flounder", "Summer flounder", "Skate", "Demersal benthivores",
+                    "Demersal piscivores", "Demersal omnivores", "Medium pelagics", "Pelagic sharks",
+                    "Large pelagics (HMS)", "Pinnipeds", "Baleen whales", "Odontocetes",
                     "Seabirds", "Detritus")
 
 scenarios$bar_label <- bar_labels_all
@@ -72,7 +72,7 @@ scenarios <- scenarios %>%
     dp_min = pmin(dp_bm1, dp_bm2, dp_bm3)
   )
 
-#Identify indices corresponding to the two groups. this is based on the Original code in Phyton. 
+#Identify indices corresponding to the two groups. this is based on the Original code in Phyton.
 #    (based on the condition abs(dp_max) >= abs(dp_min))
 idx_pos <- which(abs(scenarios$dp_max) >= abs(scenarios$dp_min))
 idx_neg <- which(abs(scenarios$dp_max) < abs(scenarios$dp_min))
@@ -104,14 +104,19 @@ scenarios_long <- scenarios_ordered %>%
                       dp_bm3 = "Senario 3")
   )
 
+scenarios_long <- scenarios_long %>%
+  group_by(bar_label) %>%
+  arrange(desc(abs(dp)), .by_group = TRUE) %>%
+  ungroup()
+
 #Build the plot using the ordered by less to more percent change
-p_stacked <- ggplot(food_web_long, aes(x = dp, y = factor(ypos, levels = unique(ypos)), fill = scenario)) +
-  geom_col() +
+p_stacked <- ggplot(scenarios_long, aes(x = dp, y = factor(ypos, levels = unique(ypos)), fill = scenario)) +
+  geom_col(position = "identity") +
   # Replace y-axis tick labels with the category names
-  scale_y_discrete(labels = food_web_ordered$bar_label) +
+  scale_y_discrete(labels = scenarios_ordered$bar_label) +
   # Set custom colors for the scenarios
-  scale_fill_manual(values = c("Senario 1" = "black", 
-                               "Senario 2" = "#E69F00", 
+  scale_fill_manual(values = c("Senario 1" = "black",
+                               "Senario 2" = "#E69F00",
                                "Senario 3" = "#56B4E9")) +
   labs(x = expression("Percent change in biomass from baseline, (%)"),
        y = NULL,
